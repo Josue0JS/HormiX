0c4c4b951cf89568d03ffd589d3c0ac163c215b6
josue <jdrafts.ex@gmail.com>
2026-02-23 09:23:55 -0500

    Creacion del repositorio HormiX

---
+++ src/main/java/com/example/Hormix/HormixApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hormix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HormixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HormixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hormix/HormixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hormix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HormixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
