1db23685320e4099889fc903e61328e065518277
Josue <rokylon15@gmail.com>
2026-04-13 08:48:00 -0500

    configuracion de los servicios comercios

--- src/main/java/com/example/Hormix/Services/ComercioServicio.java
+++ src/main/java/com/example/Hormix/Services/ComercioServicio.java
@@ -1,8 +1,13 @@
 package com.example.Hormix.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.Hormix.modelos.Comercio;
 import com.example.Hormix.repositories.IComercioRepositorio;
 
 @Service
@@ -10,5 +15,32 @@ public class ComercioServicio {
 
     @Autowired
     private IComercioRepositorio repositorio;
+
+    //Servicio para guardar un comercio
+    public Comercio guardar_usuario(Comercio datosComercio){
+        //validar la operacion que me estan pidiendo hacer
+        if(datosComercio.getNombre() == null || datosComercio.getNombre().isBlank()
+        || datosComercio.getNombre().isEmpty()){
+
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "El nombre del comercio es obligatorio"
+            );
+        }
+        if(datosComercio.getNit().length()<5){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "El Nit es invalido"
+            );
+        }
+
+        //Despues de las validaciones, intento guardar los datos que me enviaron
+        return repositorio.save(datosComercio);
+    }
+
+    //Servicio para listar todos los comercios en BD
+    public List<Comercio> listar_Comercios(){
+        return repositorio.findAll();
+    }
 
 }
